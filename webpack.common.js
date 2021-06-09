3061dc84faaf10c72b6240d139edf81739206d8a
Stefano Kusuma Ali <stefano8795@gmail.com>
2021-06-09 23:49:55 +0800

    end of course

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/i,
-        use: [
-          "style-loader", //3. Inject styles into DOM
-          "css-loader", //2. Turns css into js
-          "sass-loader", //1.Turns sass into css
-        ],
-      },
       { test: /\.html$/, use: ["html-loader"] },
       {
         test: /\.(png|jpe?g|gif)$/i,
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -8,4 +9,21 @@ module.exports = merge(common, {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", //3. Extract css into files
+          "css-loader", //2. Turns css into js
+          "sass-loader", //1.Turns sass into css
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 });
